6deb63b34250009ec29b7b9b0aeeffef2d72c429
Laura Abro <laura@koii.network>
2025-04-24 12:19:53 -0300

    add update audit

---
+++ worker/orca-agent/tests/stages/update_audit.py
@@ -0,0 +1,24 @@
+"""Stage for executing worker tasks."""
+
+import requests
+
+
+def prepare(runner, worker):
+    """Prepare data for worker task"""
+
+    return {
+        "taskId": runner.config.task_id,
+        "round": runner.current_round,
+    }
+
+
+def execute(runner, worker, data):
+    """Execute worker task step"""
+    url = f"{runner.config.middle_server_url}/summarizer/worker/update-audit-result"
+    response = requests.post(
+        url,
+        json=data,
+    )
+    response.raise_for_status()
+
+    return response.json()
