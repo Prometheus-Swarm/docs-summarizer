b0a7ade91675137427e83ff7ea154561c3fd3703
HermanL02 <Hermanyiqunliang@gmail.com>
2025-05-01 22:47:15 -0300

    chore: update constant url to point to production

--- worker/src/utils/constant.ts
+++ worker/src/utils/constant.ts
@@ -59,4 +59,4 @@ export const defaultBountyMarkdownFile =
 
 export const customReward = 400 * 10 ** 9; // This should be in ROE!
 
-export const middleServerUrl = "https://builder247-test.dev1.koii.network";
+export const middleServerUrl = "https://builder247-prod.dev.koii.network";
