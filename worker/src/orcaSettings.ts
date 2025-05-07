5f8bb227662529c1554f40174d01d92fb0db6453
HermanL02 <Hermanyiqunliang@gmail.com>
2025-05-06 22:47:56 -0300

    feat: update image

--- worker/src/orcaSettings.ts
+++ worker/src/orcaSettings.ts
@@ -2,7 +2,7 @@ import { TASK_ID, namespaceWrapper } from "@_koii/namespace-wrapper";
 import "dotenv/config";
 import os from "os";
 
-const imageUrl = "docker.io/labrocadabro/prometheus-summarizer:0.4";
+const imageUrl = "docker.io/hermanyiqunliang/summarizer-agent:0.5";
 
 function getHostIP() {
   const interfaces = os.networkInterfaces();
